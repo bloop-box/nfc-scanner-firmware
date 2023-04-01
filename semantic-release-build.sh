83e0136b58f15bdb35fe6399a891ab5b6092af09
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2023-04-01 14:35:33 +0200

    build: use sudo to install libudev-dev

--- semantic-release-build.sh
+++ semantic-release-build.sh
@@ -2,9 +2,9 @@
 
 VERSION="$1"
 
-apt install libudev-dev
-rustup target add thumbv6m-none-eabi
-cargo install elf2uf2-rs
+rustup target add thumbv6m-none-eabi || exit 1
+sudo apt install -y libudev-dev || exit 1
+cargo install elf2uf2-rs || exit 1
 
 sed -i '/\[package\]/,/^version = "[^"]*"$/ s/^version = "[^"]*"$/version = "'"$VERSION"'"/' Cargo.toml
 cargo build --release || exit 1
